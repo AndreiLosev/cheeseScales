0a7820a60f62807a8ead3480ca91cbefccea1db0
АСУ ТП <asutp@msz.local>
2020-04-28 17:31:50 +0300

    почти конец

--- main.py
+++ main.py
@@ -4,29 +4,56 @@ import threading
 from serialData import portScales
 
 
+nameReadWeight = 'readWeight'
+
+
+def stopRead(pScale):
+    pScale.stopRead()
+
+
+def startReadWeight(fan, fan_args, pScale):
+    pScale.startRead()
+    read = threading.Thread(target=fan, args=fan_args, name=nameReadWeight)
+    read.start()
+
+
 def main():
     root = tk.Tk()
+
     pScale = portScales()
     if len(pScale.portsNames) == 0:
         messagebox.showinfo(
             title='Авария',
             message='беда беда завите киповца, не найден COM-port'
         )
-    activPort = tk.BooleanVar
+    activPort = tk.IntVar()
+    newString = tk.StringVar()
+    newString.set(pScale.accumulatedMass)
     portSelection = [
         tk.Radiobutton(
-            text=f'для получения данных от весов использовать {x, i}',
+            text=f'для получения данных от весов использовать {x}',
             variable=activPort, value=i
         ) for i, x in enumerate(pScale.portsNames)
     ]
     for x in portSelection:
         x.pack()
+
     listTk = tk.Listbox(root, width=150)
     listTk.pack(side=tk.RIGHT)
     frame = tk.Frame(root)
     frame.pack(side=tk.LEFT)
-    buttonStart = tk.Button(frame, text='Старт')
-    buttonStop = tk.Button(frame, text='Стоп')
+    buttonStart = tk.Button(
+        frame, text='Старт',
+        command=lambda: startReadWeight(
+            pScale.readData,
+            (pScale.portsNames[activPort.get()],),
+            pScale
+        )
+    )
+    buttonStop = tk.Button(
+        frame, text='Стоп',
+        command=lambda: stopRead(pScale)
+    )
     buttonZeroing = tk.Button(frame, text='Обнулить')
     buttonStart.pack()
     buttonStop.pack()
--- serialData.py
+++ serialData.py
@@ -5,9 +5,16 @@ import serial.tools.list_ports as serialPorts
 class portScales:
     accumulatedMass = 0
     portsNames = None
+    readContin = False
 
     def __init__(self):
         self.portsNames = self.portDiscovery()
+
+    def stopRead(self):
+        self.readContin = False
+
+    def startRead(self):
+        self.readContin = True
 
     def portDiscovery(self):
         ports = serialPorts.comports()
@@ -18,6 +25,7 @@ class portScales:
 
     def readData(self, protName):
         port = serial.Serial()
+        port.timeout = 0
         port.port = protName
         port.baudrate = 115200
         port.bytesize = 7
@@ -26,8 +34,15 @@ class portScales:
         port.open()
         while True:
             try:
-                s = port.readline()
-                print(s)
+                if self.readContin:
+                    s = port.readline()
+                    if bool(s):
+                        self.accumulatedMass += 1
+                        print(f'{s} ::: {self.accumulatedMass}')
+                    else:
+                        continue
+                else:
+                    break
             except BaseException as exc:
                 print(exc)
 
